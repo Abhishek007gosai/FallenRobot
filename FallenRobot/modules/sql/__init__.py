ce11bbfc522f22151c97933f8a6fb4da8d937f0d
NexusNetwork04 <vishalanimehub@gmail.com>
2025-09-22 15:01:43 +0530

    Update __init__.py
    Signed-off-by: NexusNetwork04 <vishalanimehub@gmail.com>

--- FallenRobot/modules/sql/__init__.py
+++ FallenRobot/modules/sql/__init__.py
@@ -5,12 +5,14 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from FallenRobot import DB_URI
 from FallenRobot import LOGGER as log
 
+# Fix old postgres:// scheme
 if DB_URI and DB_URI.startswith("postgres://"):
     DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI, client_encoding="utf8")
+    # Removed client_encoding (not supported anymore)
+    engine = create_engine(DB_URI)
     log.info("[PostgreSQL] Connecting to database......")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
